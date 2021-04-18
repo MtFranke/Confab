1d8d0c64da09efd6c72c09f649da3b486815e5cf
Mateusz <mateusz.franke@ileco.energy>
2021-04-18 12:55:40 +0200

    add speakers project to bootstraper

---
+++ src/Modules/Speakers/Confab.Modules.Speakers.Core/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: InternalsVisibleTo("Confab.Modules.Speakers.Api")]
+namespace Confab.Modules.Speakers.Core
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
