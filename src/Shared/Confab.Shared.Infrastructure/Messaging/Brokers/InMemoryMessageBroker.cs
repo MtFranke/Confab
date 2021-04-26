602748ae70f1e8645adb857ef5e1faf671bec6b8
Mateusz <mateusz.franke@ileco.energy>
2021-04-26 12:22:57 +0200

    added channel to make message broker be of type fire and forget

--- src/Shared/Confab.Shared.Infrastructure/Messaging/Brokers/InMemoryMessageBroker.cs
+++ src/Shared/Confab.Shared.Infrastructure/Messaging/Brokers/InMemoryMessageBroker.cs
@@ -3,17 +3,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Confab.Shared.Abstractions.Messaging;
 using Confab.Shared.Abstractions.Modules;
+using Confab.Shared.Infrastructure.Messaging.Dispatchers;
 
 namespace Confab.Shared.Infrastructure.Messaging.Brokers
 {
-    internal sealed class InMemoryMessageBroker: IMessageBroker
+    internal sealed class InMemoryMessageBroker : IMessageBroker
     {
-
         private readonly IModuleClient _moduleClient;
+        private readonly IAsyncMessageDispatcher _asyncMessageDispatcher;
+        private readonly MessagingOptions _messagingOptions;
 
-        public InMemoryMessageBroker(IModuleClient moduleClient)
+        public InMemoryMessageBroker(IModuleClient moduleClient, IAsyncMessageDispatcher asyncMessageDispatcher,
+            MessagingOptions messagingOptions)
         {
             _moduleClient = moduleClient;
+            _asyncMessageDispatcher = asyncMessageDispatcher;
+            _messagingOptions = messagingOptions;
         }
 
         public async Task PublishAsync(params IMessage[] messages)
@@ -31,14 +36,19 @@ namespace Confab.Shared.Infrastructure.Messaging.Brokers
             }
 
             var tasks = new List<Task>();
-            
+
             foreach (var message in messages)
             {
+                if (_messagingOptions.UseBackgroundDispatcher)
+                {
+                    await _asyncMessageDispatcher.PublishAsync(message);
+                    continue;
+                }
+
                 tasks.Add(_moduleClient.PublishAsync(message));
             }
 
             await Task.WhenAll(tasks);
-
         }
     }
 }
