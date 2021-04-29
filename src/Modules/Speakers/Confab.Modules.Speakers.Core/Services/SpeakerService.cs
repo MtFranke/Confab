fa699edc7e8fdd1ba19a1ae6041a3f98f7afee95
Mateusz <mateusz.franke@ileco.energy>
2021-04-29 15:13:17 +0200

    external event integration on speaker created in agendas

--- src/Modules/Speakers/Confab.Modules.Speakers.Core/Services/SpeakerService.cs
+++ src/Modules/Speakers/Confab.Modules.Speakers.Core/Services/SpeakerService.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Confab.Modules.Speakers.Core.DTO;
+using Confab.Modules.Speakers.Core.Events;
 using Confab.Modules.Speakers.Core.Exceptions;
 using Confab.Modules.Speakers.Core.Mappings;
 using Confab.Modules.Speakers.Core.Repositories;
+using Confab.Shared.Abstractions.Messaging;
 
 namespace Confab.Modules.Speakers.Core.Services
 {
     internal class SpeakerService : ISpeakerService
     {
         private readonly ISpeakerRepository _speakerRepository;
+        private readonly IMessageBroker _messageBroker;
 
-        public SpeakerService(ISpeakerRepository speakerRepository)
+        public SpeakerService(ISpeakerRepository speakerRepository, IMessageBroker messageBroker)
         {
             _speakerRepository = speakerRepository;
+            _messageBroker = messageBroker;
         }
 
         public async Task<IEnumerable<SpeakerDto>> BrowseAsync()
@@ -39,6 +43,7 @@ namespace Confab.Modules.Speakers.Core.Services
             }
 
             await _speakerRepository.AddAsync(speaker.AsEntity());
+            await _messageBroker.PublishAsync(new SpeakerCreated(speaker.Id, speaker.FullName));
         }
 
         public async Task UpdateAsync(SpeakerDto speaker)
